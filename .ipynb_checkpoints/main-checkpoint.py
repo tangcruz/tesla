6f3e0884e39479cda7ca0f14b559a8e4a501f389
tangcruz <cruz@kidsedu.ai>
2024-07-28 13:13:04 +0800

    test telegram

--- .ipynb_checkpoints/main-checkpoint.py
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -1,10 +1,15 @@
 from flask import Flask
+import telegram
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World1!'
+    return 'Hello, World!'
+
+@app.route('/version')
+def version():
+    return f'python-telegram-bot version: {telegram.__version__}'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-from bot import run_bot
+from flask import Flask
+import telegram
 
-if __name__ == "__main__":
-    run_bot()
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+@app.route('/version')
+def version():
+    return f'python-telegram-bot version: {telegram.__version__}'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
